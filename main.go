30d490accfb8c5ec2d07ea47f6415e7a473f5ff7
0xF0D0 <goodbumsu@gmail.com>
2020-04-16 20:00:47 +0900

    add glut framework
    Signed-off-by: 0xF0D0 <goodbumsu@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/0XF0D0/glut"
+)
 
 func main() {
-	fmt.Println("hello go emulator!!")
+	glut.Init()
+	glut.InitDisplayMode(glut.SINGLE | glut.RGBA)
+	glut.InitWindowSize(320, 240)
+	glut.CreateWindow("c8 emulator")
+	glut.ReshapeFunc(reshape)
+	glut.DisplayFunc(display)
+	glut.MainLoop()
+}
+
+func reshape(width, height int) {
+	fmt.Println("reshape")
+}
+
+func display() {
+	fmt.Println("display")
 }
