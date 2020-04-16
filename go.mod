5f9efc869299d4f1c6effe35584a39aea260e0ca
0xF0D0 <goodbumsu@gmail.com>
2020-04-16 22:56:50 +0900

    guide glut code
    Signed-off-by: 0xF0D0 <goodbumsu@gmail.com>

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/0xF0D0/c8emulator
 
 go 1.14
 
-require (
-	github.com/0XF0D0/glut v0.0.0-20200415134840-61e3a12a2a37
-	github.com/vbsw/glut v0.0.0-20170827183444-24883f536ef1
-)
+require github.com/0XF0D0/glut v0.0.0-20200416132635-7002b508f2f4
--- main.go
+++ main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"unsafe"
 
 	"github.com/0XF0D0/glut"
 )
 
+var screenData [32][64][3]byte
+var cnt int = 0
+
 func main() {
 	glut.Init()
 	glut.InitDisplayMode(glut.SINGLE | glut.RGBA)
-	glut.InitWindowSize(320, 240)
+	glut.InitWindowSize(640, 320)
 	glut.CreateWindow("c8 emulator")
 	glut.ReshapeFunc(reshape)
 	glut.DisplayFunc(display)
+	glut.IdleFunc(display)
+
+	glut.TexImage2DRGBByte(64, 32, unsafe.Pointer(&screenData))
+	glut.SetTexParameteri()
+	glut.EnableTexture()
+
 	glut.MainLoop()
+
+}
+
+func draw() {
+	glut.Clear()
+
+	for i := 0; i < 32; i++ {
+		for j := 0; j < 64; j++ {
+			for k := 0; k < 3; k++ {
+				screenData[i][j][k] = 122
+			}
+		}
+	}
+
+	glut.TexSubImage2DRGBByte(64, 32, unsafe.Pointer(&screenData))
+	glut.DrawTexture(640, 320)
+	glut.SwapBuffers()
 }
 
 func reshape(width, height int) {
-	fmt.Println("reshape")
+	glut.ClearColor(0.0, 0.0, 0.5, 0.0)
+	glut.SetMatrixModeProjection()
+	glut.LoadIdentity()
+	glut.Ortho2D(width, height)
+	glut.SetMatrixModeModelView()
+	glut.ViewPort(width, height)
 }
 
 func display() {
-	fmt.Println("display")
+	if cnt < 100000 {
+		cnt++
+		return
+	} else {
+		draw()
+		cnt = 0
+	}
 }
